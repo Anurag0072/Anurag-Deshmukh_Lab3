1d8160c597486197db79b28225f3962541c3bb1a
Anurag Deshmukh <48847887+Anurag0072@users.noreply.github.com>
2022-07-24 21:22:45 +0530

    Add files via upload

---
+++ lab3/balancingBrackets/brackets.java
@@ -0,0 +1,61 @@
+package balancingBrackets;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class brackets {
+	
+   public static void main(String[] args)
+	   {
+	      String expression;
+	      int i, length;
+	      char ch;
+	      Scanner s = new Scanner(System.in);
+	      
+	      System.out.print("Enter the Expression: ");
+	      expression = s.nextLine();
+	      
+	      Stack<Character> stack = new Stack<Character>();
+	      length = expression.length();
+	      
+	      for(i=0; i<length; i++)
+	      {
+	         ch = expression.charAt(i);
+	         if(ch=='(' || ch=='{' || ch=='[')
+	         {
+	            stack.push(ch);
+	         }
+	         else if(ch==')')
+	         {
+	            if(stack.isEmpty() || stack.pop() != '(')
+	            {
+	               System.out.println("\nThe entered Strings do not contain Balanced Brackets\r\n"
+	               		+ "");
+	               return;
+	            }
+	         }
+	         else if(ch=='}')
+	         {
+	            if(stack.isEmpty() || stack.pop() != '{')
+	            {
+	               System.out.println("\nThe entered Strings do not contain Balanced Brackets\r\n"
+	               		+ "");
+	               return;
+	            }
+	         }
+	         else if(ch==']')
+	         {
+	            if(stack.isEmpty() || stack.pop() != '[')
+	            {
+	               System.out.println("\nThe entered Strings do not contain Balanced Brackets\r\n"
+	               		+ "");
+	               return;
+	            }
+	         }
+	      }
+	      if(stack.isEmpty())
+	         System.out.println("\nThe entered String has Balanced Brackets.");
+	   }
+	}
+
+
